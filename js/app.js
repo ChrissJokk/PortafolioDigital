8b45b62459ad13948c976974769ddc3449a84348
Christian <christian.tapia@wisetrack.cl>
2023-07-13 18:35:11 -0400

    Error- Tooltip
    Correguir- Tooltip

--- js/app.js
+++ js/app.js
@@ -12,27 +12,6 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
-
-/*Fix*/
-$('.navbar-nav>li>a').on('click', function(){
+function collapse_nav(){
     $('.navbar-collapse').collapse('hide');
-});
-
-
-
-
-//function click_menu() {  
-//     const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         // console.log(entry)
-//         if (entry.isIntersecting){
-//             entry.target.classList.add('show');
-//         }else{
-//             entry.target.classList.remove('show');
-//         }
-//     });
-// });
-
-// const hiddenElements = document.querySelectorAll('.hidden');
-// hiddenElements.forEach((el) => observer.observe(el));
-//}  
+}
